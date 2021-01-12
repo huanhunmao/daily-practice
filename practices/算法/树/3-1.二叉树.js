adae3e31fc95f7eae205ca00c33eb3bef3bd1909
huanhunmao <1311926640@qq.com>
2021-01-12 19:17:46 +0800

    add--

---
+++ practices/算法/树/3-1.二叉树.js
@@ -0,0 +1,21 @@
+const bt = {
+  val: 3,
+  left: {
+    val: 9,
+    left: null,
+    right: null,
+  },
+  right: {
+    val: 20,
+    left: {
+      val: 15,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 7,
+      left: null,
+      right: null,
+    },
+  },
+};
